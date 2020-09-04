4473b8b07c61e9b76664b9adfc927a7de9980410
Ayuo Buba <ayobuba@gmail.com>
2020-09-04 15:45:07 +0100

    initial push

---
+++ command.js
@@ -0,0 +1,109 @@
+const { Pool, Client } = require('pg');
+const { BigQuery } = require('@google-cloud/bigquery');
+const bigquery = new BigQuery();
+
+const pool = new Pool({
+    user: "user",
+    host: "localhost",
+    database: "sample_analytics",
+    password: "12345",
+    port: "5433"
+});
+
+function eventDatetoTimestamp(a) {
+    b = '' + a
+    let yyyy = b.substring(0,4)
+    let mm = b.substring(4,6)
+    let dd = b.substring(6,8)
+
+    let result = yyyy +"-" + mm+"-"+ dd + " 00:00:00"
+
+    return result
+}
+
+
+async function getFromBigQuery(req,res) {
+    const query = `SELECT * FROM \`bigquery-public-data.google_analytics_sample.ga_sessions_20170801\`limit 1000`;
+    const options = {
+        query: query,
+        location: 'US',
+    };
+
+    const [job] = await bigquery.createQueryJob(options);
+    console.log(`Job ${job.id} started.`);
+
+    const [rows] = await job.getQueryResults();
+    rows.forEach(row => console.log(row));
+
+    let array = [];
+    for (let i = 0; i < rows.length; i++) {
+        let data = rows[i];
+        let model = {
+            visitId: data.visitId,
+            date: eventDatetoTimestamp(data.date),
+            totals: data.totals,
+            device: data.device,
+            geoNetwork: data.geoNetwork
+        }
+        array.push(model);
+    }
+
+    for(let a = 0; a < array.length; a++) {
+        let values = Object.values(array[a]);
+
+        let queryInsert = 'INSERT INTO sample_analytics (visitId,date,totals,device,geoNetwork) VALUES ($1,$2,$3,$4,$5)';
+        pool.query(queryInsert, values).then(result => {
+            return res.json ({ code : 200, success: true, message: "Success Insert Data From Bigquery" })
+        }).catch(error => {
+            return res.json ({ code : 500, success: true, message: "Ooopsss!, Something is wrong", data: error })
+        })
+    }
+}
+
+async function getFromPostgres(req,res) {
+    try {
+        const query = 'SELECT * FROM sample_analytics'
+
+        const result = await pool.query(query);
+        return res.json ({ code : 201, success: true, message: "Success Get Data", data: result.rows })
+    } catch (error) {
+        return res.json ({ code : 501, success: true, message: "Ooopsss!, Something is wrong", data: error })
+    }
+}
+
+async function createTable(req,res) {
+    try {
+        const query = `CREATE TABLE sample_analytics (
+            visitId integer,
+            date timestamp,
+            totals json,
+            device json,
+            geoNetwork json
+        )`
+
+        const result = await pool.query(query)
+        return res.json ({ code : 201, success: true, message: "Success Create Table", data: result })
+    } catch (error) {
+        return res.json ({ code : 502, success: true, message: "Ooopsss!, Something is wrong", data: error })
+    }
+
+}
+
+async function dropTable(req,res){
+    try {
+        let query = `DROP TABLE sample_analytics`
+        const result = await pool.query(query);
+
+        return res.json ({ code : 201, success: true, message: "Success Drop Table", data: result })
+    } catch (error) {
+        return res.json ({ code : 503, success: true, message: "Ooopsss!, Something is wrong", data: error })
+    }
+}
+
+module.exports = {
+    getFromBigQuery,
+    getFromPostgres,
+    createTable,
+    dropTable
+}
+
